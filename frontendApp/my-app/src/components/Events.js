955d347edba7a62ae0ad0d54d127c069c32a900f
Edemowns <elknyanyo@st.ug.edu.gh>
2024-07-24 12:53:52 -1200

    Initial Commit

---
+++ frontendApp/my-app/src/components/Events.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Events = () => {
+  return (
+    <div className="events">
+      <h2>Upcoming Events</h2>
+      <p>This is the Events page content.</p>
+    </div>
+  );
+};
+
+export default Events;
