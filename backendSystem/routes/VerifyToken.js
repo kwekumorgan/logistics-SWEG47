57aca30f0288f319accda5c1604b11f13be60125
Pkay28748 <pnkarthur@st.ug.edu.gh>
2024-07-29 07:48:25 -0400

    Verify token error resolved

--- backendSystem/routes/VerifyToken.js
+++ backendSystem/routes/VerifyToken.js
@@ -3,33 +3,33 @@ like password and username update to make sure only verified users make
 those changes */
 
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const verifyToken = (req,res,next)=>{
-    const authHeader = req.headers.token
-    if(authHeader){
-        // Verify function
-        jwt.verify(token, 'Logistics47',(err,user)=>{
-            if(err) return res.status(403).json('Token is invalid');
-            req.user= user;
+
+// Middleware to verify token
+const verifyToken = (req, res, next) => {
+    const token = req.headers.token;
+    if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, (err, user) => { // user here is just a parameter
+            if (err) res.status(403).json("Token is not valid!");
+            req.user = user;
             next();
         });
-
-    } else{
-        return res.status(401).json('You are not authenticated');
+    } else {
+        return res.status(401).json("You are not authenticated!");
     }
 };
 
-const verifyTokenAndAuthorization =(req,res,next)=>{
-
-    verifyToken(req,res,()=>{
-        if(req.user.id == req.params.id || req.user.isAdmin){
-            next()
-        }else{
-            res.status(403).json('Sorry you are not allowed to do this changes ')
+// Middleware to verify token and authorization
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next();
+        } else {
+            res.status(403).json("You are not allowed to do that!");
         }
-    }
-      
+    });
+};
 
-    );
-  
-module.exports = {verifyToken};
+module.exports = { verifyToken, verifyTokenAndAuthorization };
--- backendSystem/routes/customerRoute.js
+++ backendSystem/routes/customerRoute.js
@@ -1,29 +1,27 @@
-// Contains all  http request the customers can make 
-
 const router = require('express').Router();
-const {verifyToken, verifyTokenAndAuthorization} = require('./VerifyToken');
+const CryptoJS = require('crypto-js');
+const { verifyTokenAndAuthorization } = require('./VerifyToken');
+const Customer = require('../models/customer')
+const dotenv = require('dotenv');
+dotenv.config();
 
-// UPDATE
-router.put('/:id',verifyTokenAndAuthorization,async(req,res)=>{
-    if(req.body.password){
-        req.body.password = CryptoJS.AES.encrypt(req.body.password, 'Logistics47').toString()
+// UPDATE CREDENTIALS
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
     }
 
-    try{
-        const updatedCustomer = await user.findByIdAndUpdate(req.params.id,{
-            $set : req.body,
-        },
-            {new:true});
+    try {
+        const updatedCustomer = await Customer.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
 
         res.status(200).json(updatedCustomer);
-    } catch(err){
-        res.status(500).json(err);
+    } catch (err) {
+        res.status(500).json({ message: 'Error updating customer credentials', error: err });
     }
- 
-    
 });
 
-
-
-
-module.exports= router;
+module.exports = router;
