62d54716be9a02fcfb05ebf616cc8b1e59fc3436
Edemowns <elknyanyo@st.ug.edu.gh>
2024-08-19 09:47:57 -1200

    Frontend Update

--- frontendApp/my-app/src/components/LoginRegister.jsx
+++ frontendApp/my-app/src/components/LoginRegister.jsx
@@ -1,41 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import "./LoginRegister.css";
-import "./Header.css"; 
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
-import KashLogo from '../Media/KASHLOGO1.jpg';
-import SearchIcon from '../Media/Search.png';
+import "./LoginRegister.css";
 
 const LoginRegister = () => {
-  useEffect(() => {
-    function handleScroll() {
-      const header = document.querySelector('.header');
-      const navbarHeight = document.querySelector('.ribbon').offsetHeight;
-      if (window.scrollY > navbarHeight) {
-        header.style.position = 'fixed';
-        header.style.top = '0';
-      } else {
-        header.style.position = 'relative';
-        header.style.top = '';
-      }
-    }
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const handleSearch = () => {
-    alert('Perform search operation');
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      handleSearch();
-    }
-  };
+  const navigate = useNavigate();
   const [action, setAction] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
@@ -53,89 +22,72 @@ const LoginRegister = () => {
 
   const handleLogin = (e) => {
     e.preventDefault();
+    // Perform login logic here
     console.log('Username:', username);
     console.log('Password:', password);
     setUsername('');
     setPassword('');
+    navigate('/'); // Redirect to home page after login
   };
 
   const handleRegister = (e) => {
     e.preventDefault();
+    // Perform registration logic here
     console.log('Username:', username);
     console.log('Email:', email);
     console.log('Password:', password);
     setUsername('');
     setEmail('');
     setPassword('');
+    navigate('/'); // Redirect to home page after registration
   };
 
   return (
-    <div>
-      <header className="header">
-      <img src={KashLogo} height="80" alt="Department Of Computer Science" />
-      <Link to="/">Home</Link>
-      <Link to="/product">Product</Link>
-      <Link to="/Carts">Carts</Link>
-      <Link to="/About">About</Link>
-     
-      <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="Search"
-          onKeyPress={handleKeyPress}
-        />
-        <button className="search-button" onClick={handleSearch}>
-          <img src={SearchIcon} alt="Search" />
-        </button>
+    <div className={`login-register-wrapper ${action === 'active' ? 'active' : ''}`}>
+      <div className='form-box login'>
+        <form onSubmit={handleLogin}>
+          <h1>Login</h1>
+          <div className='input-box'>
+            <input type="text" placeholder='Username' required value={username} onChange={(e) => setUsername(e.target.value)} />
+            <FaUser className='icon' />
+          </div>
+          <div className='input-box'>
+            <input type="password" placeholder='Password' required value={password} onChange={(e) => setPassword(e.target.value)} />
+            <FaLock className='icon' />
+          </div>
+          <div className="remember-forgot">
+            <label><input type="checkbox" /> Remember me</label>
+            <button type="button" className="link-button">Forgot password?</button>
+          </div>
+          <button type="submit">Login</button>
+          <div className="register-link">
+            <p>Don't have an account? <button type="button" className="link-button" onClick={registerLink}>Register</button></p>
+          </div>
+        </form>
       </div>
-    </header>
-      <div className={`login-register-wrapper ${action === 'active' ? 'active' : ''}`}>
-        <div className='form-box login'>
-          <form onSubmit={handleLogin}>
-            <h1>Login</h1>
-            <div className='input-box'>
-              <input type="text" placeholder='Username' required value={username} onChange={(e) => setUsername(e.target.value)} />
-              <FaUser className='icon' />
-            </div>
-            <div className='input-box'>
-              <input type="password" placeholder='Password' required value={password} onChange={(e) => setPassword(e.target.value)} />
-              <FaLock className='icon' />
-            </div>
-            <div className="remember-forgot">
-              <label><input type="checkbox" /> Remember me</label>
-              <button type="button" className="link-button">Forgot password?</button>
-            </div>
-            <button type="submit">Login</button>
-            <div className="register-link">
-              <p>Don't have an account? <button type="button" className="link-button" onClick={registerLink}>Register</button></p>
-            </div>
-          </form>
-        </div>
-        <div className={`form-box register ${action === 'active' ? 'active' : ''}`}>
-          <form onSubmit={handleRegister}>
-            <h1>Registration</h1>
-            <div className='input-box'>
-              <input type="text" placeholder='Username' required value={username} onChange={(e) => setUsername(e.target.value)} />
-              <FaUser className='icon' />
-            </div>
-            <div className='input-box'>
-              <input type="text" placeholder='Email' required value={email} onChange={(e) => setEmail(e.target.value)} />
-              <FaEnvelope className='icon' />
-            </div>
-            <div className='input-box'>
-              <input type="password" placeholder='Password' required value={password} onChange={(e) => setPassword(e.target.value)} />
-              <FaLock className='icon' />
-            </div>
-            <div className="remember-forgot">
-              <label><input type="checkbox" /> I agree to the terms & conditions</label>
-            </div>
-            <button type="submit">Register</button>
-            <div className="register-link">
-              <p>Already have an account? <button type="button" className="link-button" onClick={loginLink}>Login</button></p>
-            </div>
-          </form>
-        </div>
+      <div className={`form-box register ${action === 'active' ? 'active' : ''}`}>
+        <form onSubmit={handleRegister}>
+          <h1>Registration</h1>
+          <div className='input-box'>
+            <input type="text" placeholder='Username' required value={username} onChange={(e) => setUsername(e.target.value)} />
+            <FaUser className='icon' />
+          </div>
+          <div className='input-box'>
+            <input type="text" placeholder='Email' required value={email} onChange={(e) => setEmail(e.target.value)} />
+            <FaEnvelope className='icon' />
+          </div>
+          <div className='input-box'>
+            <input type="password" placeholder='Password' required value={password} onChange={(e) => setPassword(e.target.value)} />
+            <FaLock className='icon' />
+          </div>
+          <div className="remember-forgot">
+            <label><input type="checkbox" /> I agree to the terms & conditions</label>
+          </div>
+          <button type="submit">Register</button>
+          <div className="register-link">
+            <p>Already have an account? <button type="button" className="link-button" onClick={loginLink}>Login</button></p>
+          </div>
+        </form>
       </div>
     </div>
   );
