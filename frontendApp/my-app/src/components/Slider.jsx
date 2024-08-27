4fa8458e98c57d57250df530c13870dc5009990e
Edemowns <151376933+Edemowns@users.noreply.github.com>
2024-08-27 03:37:47 +0000

    testing fonts (#16)

--- frontendApp/my-app/src/components/Slider.jsx
+++ frontendApp/my-app/src/components/Slider.jsx
@@ -5,6 +5,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import video from '../Media/VIDEO.mp4';
 import image from '../Media/BarretWard1.jpg';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate instead of useHistory
 
 const ImageSlider = () => {
   const settings = {
@@ -18,14 +19,25 @@ const ImageSlider = () => {
     adaptiveHeight: true,
   };
 
+  const navigate = useNavigate(); // Use useNavigate for navigation
+
+  const navigateToAbout = () => {
+    navigate('/about'); // Use navigate instead of history.push
+  };
+
   return (
     <div className="slider-wrapper">
       <Slider {...settings}>
         <div className="image-container">
           <img src={image} alt="Barret Ward" className="full-screen-image" />
+          <div className="text">Secure Your Seals With Confidence</div>
+          <div className='text2'>Reliable sealing solutions for every shipping need.</div>
+          <button className="button" onClick={navigateToAbout}>Learn More</button>
         </div>
         <div className="video-container">
           <video src={video} autoPlay loop muted playsInline className="full-screen-video" />
+          <div className="text">Secure Your Seals With Confidence</div>
+          <div className='text2'>Reliable sealing solutions for every shipping need.</div>
         </div>
       </Slider>
     </div>
