6f5c1bffaa55376b1d9c5998a94b000c1c9c3249
Edemowns <elknyanyo@st.ug.edu.gh>
2024-08-28 22:53:58 -1200

    gggfg

--- frontendApp/my-app/src/components/ProductsContainer.js
+++ frontendApp/my-app/src/components/ProductsContainer.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import product_data from '../components/product_data'; // Import product data
-import './ProductsContainer.css'; // Ensure this file has the styling
-import { useCart } from './CartContext'; // Import the useCart hook
+import product_data from '../components/product_data';
+import './ProductsContainer.css';
+import { useCart } from '../components/CartContext';
 
 const ProductsContainer = () => {
-  const { addToCart } = useCart(); // Destructure addToCart from the CartContext
+  const { addToCart } = useCart();
 
-  // Combine all product categories into a single array
   const allProducts = [
     ...product_data.boltSeals,
     ...product_data.cableSeals,
     ...product_data.plasticSeals,
   ];
 
-  // Handler to add the product to the cart
   const handleAddToCart = (product, event) => {
     addToCart(product);
-
     const button = event.currentTarget;
     button.classList.add('clicked');
-
     setTimeout(() => {
       button.classList.remove('clicked');
-    }, 200); // The duration should match the CSS animation duration
+    }, 200);
   };
 
   return (
@@ -35,9 +31,10 @@ const ProductsContainer = () => {
             <div className="product-name text">{product.product_name}</div>
             <div className="product-description text">{product.description}</div>
             <div className="product-price text">{`${product.currency} ${product.price}`}</div>
-            <button 
-              className="button" 
-              onClick={(event) => handleAddToCart(product, event)}>
+            <button
+              className="button"
+              onClick={(event) => handleAddToCart(product, event)}
+            >
               Add to Cart
             </button>
           </div>
